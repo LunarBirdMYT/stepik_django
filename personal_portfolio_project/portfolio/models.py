63168594efbb2f1d613a5335263722deffc94d9e
LunarBirdMYT <78801669+LunarBirdMYT@users.noreply.github.com>
2022-08-04 21:11:34 +0300

    Portfolio project
    Создаем второй проект)

---
+++ personal_portfolio_project/portfolio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=100)
+    description = models.CharField(verbose_name='Описание', max_length=250)
+    image = models.ImageField(verbose_name='Изображение', upload_to='portfolio/images')
+    url = models.URLField(verbose_name='Ссылка', blank=True)
