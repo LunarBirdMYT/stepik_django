16e8f27fe8e15d24e49a24f1b18a59d0159610da
LunarBirdMYT <pav.mgk50@mail.ru>
2022-09-09 15:33:43 +0300

    Добавили модель блога и поправили странику.

--- personal_portfolio_project/blog/models.py
+++ personal_portfolio_project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    description = models.TextField(verbose_name='Описание')
+    created = models.DateField(verbose_name='Дата создания')
+
+    def __str__(self) -> str:
+        return self.title
