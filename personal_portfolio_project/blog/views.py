2da77b37cbced9e70cdf0b6363d1d4d2a07211bd
LunarBirdMYT <pav.mgk50@mail.ru>
2022-09-09 16:04:35 +0300

    Вьюхи и персональная страница для блога.

--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Blog
 
 def all_blogs(request):
     # Но тут лучше бы использовать пагинатор
     blogs = Blog.objects.order_by('-created')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
