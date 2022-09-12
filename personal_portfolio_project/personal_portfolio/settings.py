f86155f723e60630c32f275e64ceb2298a66f02f
LunarBirdMYT <pav.mgk50@mail.ru>
2022-09-12 16:04:09 +0300

    Убираем ключ.

--- personal_portfolio_project/personal_portfolio/settings.py
+++ personal_portfolio_project/personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!=ake31cqt*o5p1(b*0s)e9y%w-@00b8*^qcdg2p**x6@%fuab'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
